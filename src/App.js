d4c12588604187ce482569f9a9f243ceb63e777d
Dyandra Allen <dallen9@binghamton.edu>
2021-08-04 14:27:13 -0400

    Reorganization: own repo

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, {useEffect, useRef, useState} from "react";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import usePrevious from "./usePrevious";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  /*[a state, function that updates it]*/
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter} 
+    />
+  ));
+
+  function addTask(name){
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]); /*spread syntax - copies existing arr (tasks) and adds a newTask obj to arr*/
+  }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task =>{
+      // if this task has the same ID as the edited task
+      if(id === task.id){
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return{...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function editTask(id, newName){
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id){
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter]) 
+    .map(task => (
+      <Todo 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    )
+    /* filter - we access the value in FILTER_MAP that corresponds to the key of our filter state*/
+  );
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if(tasks.length - prevTaskLength === -1){
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      {/*User enters a new task*/}
+      <Form addTask={addTask}/>
+
+      {/*
+        visually-hidden class
+        - Useful for assistive tech
+        - Visual cues help sighted users
+        - Thus, need more explanation for screen reader users
+      */}
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}> 
+        {headingText}
+      </h2>
+        {/*
+          role="list" for ul
+          - Usually implicit
+          - The styles we add break that list functionality
+          - Explicitly stating the role returns the functionality
+
+          aria-labelledby
+          - For assistive tech
+          - Assoc first heading ("list-heading") w/ list
+        */}
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
